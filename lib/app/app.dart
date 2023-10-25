c90e80387f51136819ec663de7ff2af2e26c35df
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2023-10-25 19:10:19 +0300

    models added

---
+++ lib/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:news_application/app/views/homepage/homepage_view.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  final String appTitle = "News App";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
